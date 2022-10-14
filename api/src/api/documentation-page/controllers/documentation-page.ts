e7ee1c3ea3e59b0d2891d618014bc98bc2e49de9
Dusica Vasic <d.vasic@vegait.rs>
2022-10-15 00:16:20 +0200

    Added about us, documentation and publication pages

---
+++ api/src/api/documentation-page/controllers/documentation-page.ts
@@ -0,0 +1,7 @@
+/**
+ * documentation-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::documentation-page.documentation-page');
