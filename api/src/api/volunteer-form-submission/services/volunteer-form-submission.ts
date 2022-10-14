735520c38a0bf1385ea33d75876a4d5635799218
Dusica Vasic <d.vasic@vegait.rs>
2022-10-14 22:40:53 +0200

    Added homepage, blog page and project page and related components

---
+++ api/src/api/volunteer-form-submission/services/volunteer-form-submission.ts
@@ -0,0 +1,7 @@
+/**
+ * volunteer-form-submission service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::volunteer-form-submission.volunteer-form-submission');
---
+++ api/src/api/work-program-page/services/work-program-page.ts
@@ -0,0 +1,7 @@
+/**
+ * work-program-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::work-program-page.work-program-page');
