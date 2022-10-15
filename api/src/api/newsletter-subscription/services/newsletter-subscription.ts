da0ac90e187826add16553ece5a8f0ae6e854be8
Aleksandar Mudrić <a.mudric@vegait.rs>
2022-10-15 16:56:34 +0200

    Updated logic for subscribe to newsletter and added unsubscribe

---
+++ api/src/api/newsletter-subscription/services/newsletter-subscription.ts
@@ -0,0 +1,9 @@
+/**
+ * newsletter-subscription service
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreService(
+  "api::newsletter-subscription.newsletter-subscription"
+);
