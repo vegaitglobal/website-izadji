0b575caa077478ad133b338459e9157eb88f5245
Dusica Vasic <d.vasic@vegait.rs>
2022-10-15 18:04:42 +0200

    Made trending articles component

---
+++ izadji/src/components/TrendingArticles/TrendingArticles.tsx
@@ -0,0 +1,34 @@
+import TrendingArticle from '../TrendingArticle/TrendingArticle';
+import styles from './TrendingArticles.module.scss';
+
+export type ProjectSlideProps = {
+  imageSrc: string;
+  date: string;
+  title: string;
+};
+
+type ProjectSliderProps = {
+  slides: ProjectSlideProps[];
+};
+const TrendingArticles = ({ slides }: ProjectSliderProps): JSX.Element => {
+  return (
+    <div className={styles.feed}>
+      <div className={styles.wrap}>
+        <h2 className={`${styles.feed__title} ${styles.section__title}`}>
+          Naslov
+        </h2>
+        <div className={styles.feed__list}>
+          {slides.map((slide) => (
+            <TrendingArticle
+              key={slide.title}
+              imageSrc={slide.imageSrc}
+              date={slide.date}
+              title={slide.title}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default TrendingArticles;
