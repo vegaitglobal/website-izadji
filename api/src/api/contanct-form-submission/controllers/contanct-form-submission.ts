f6bddef2c90500ec518681728d29e49623ea0748
Bojana Kliska <52880798+KliskaB@users.noreply.github.com>
2022-10-16 16:54:12 +0200

    Send mail on contact form

--- api/src/api/contanct-form-submission/controllers/contanct-form-submission.ts
+++ api/src/api/contanct-form-submission/controllers/contanct-form-submission.ts
@@ -5,13 +5,20 @@
 import mailService from "../../email/services/email-service"
 import { factories } from '@strapi/strapi'
 
+const TITLE = "Novi kontakt"
+const SUBJECT = "Novi kontakt"
+
+const generateMailContent = (name, phoneNumber, text, email) => {
+    return `Ime: ${name} <br/> Email: ${email} <br/> Broj telefona: ${phoneNumber} <br/> Poruka: ${text}`
+  }
 export default factories.createCoreController('api::contanct-form-submission.contanct-form-submission', ({strapi}) => ({
     async create(ctx) {
         const entity = strapi.entityService.create('api::contanct-form-submission.contanct-form-submission')
         console.log("submission controller")
-        ctx.body = "Hello";
+        ctx.body = "Success";
         const {name, phone, content, email} = ctx.request.body.data
-        console.log(phone)
+        let mailContent = generateMailContent(name, phone, content, email)
+        mailService.sendMail(process.env['SMTP_CONTACT_EMAIL'], email, TITLE, SUBJECT, mailContent)
         
     }
 }));
