8f08e6de238b0b5619a2f439a1f711e814203d74
Lazar.Jovic <lazar.jovic@doctorcareanywhere.com>
2022-10-26 16:57:48 +0200

    Implement Volunteering form submitting and toast messages. Correct endpoint url needs to be added once it is implemented.

--- izadji/src/components/VolunteeringForm/VolunteeringForm.tsx
+++ izadji/src/components/VolunteeringForm/VolunteeringForm.tsx
@@ -1,3 +1,7 @@
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import formService from '../../services/formService';
+import Toast from '../shared/Toast/Toast';
 import styles from './VolunteeringForm.module.scss';
 
 export type VolunteeringFormProps = {
@@ -11,6 +15,24 @@ const VolunteeringForm = ({
   description,
   buttonText,
 }: VolunteeringFormProps): JSX.Element => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // TODO: Review response and error handling (messages)
+  const onSubmit = (data: any) => {
+    formService
+      .sendEmailData(data)
+      .then(() => toast.success('Uspešna prijava na projekat.'))
+      .catch(() => {
+        toast.error(
+          'Trenutno postoje smetnje prilikom procesa prijave. Molimo, pokušajte ponovo.'
+        );
+      });
+  };
+
   return (
     <section className={styles.contact__block}>
       <div className={styles.wrap}>
@@ -25,7 +47,10 @@ const VolunteeringForm = ({
             <span className={styles.contact__block__arrow}></span>
           </div>
 
-          <form className={`${styles.contact__block__form} ${styles.form}`}>
+          <form
+            className={`${styles.contact__block__form} ${styles.form}`}
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <div className={styles.form__inner}>
               <div className={styles.form__col}>
                 <label
@@ -37,7 +62,13 @@ const VolunteeringForm = ({
                   id="vol-form-name"
                   className={styles.form__input}
                   placeholder="Ime"
+                  {...register('name', { required: true })}
                 />
+                {errors.name && (
+                  <p className={styles.form__error__message}>
+                    Polje <i>Ime</i> je obavezno za unos
+                  </p>
+                )}
                 <label
                   htmlFor="vol-form-phone"
                   className={styles.sr__only}
@@ -45,7 +76,6 @@ const VolunteeringForm = ({
                 {/* TODO validacija za telefon */}
                 <input
                   type="tel"
-                  pattern="(\+381)?(\s|-)?6(([0-6]|[8-9])\d{8}|(77|78)\d{7}){1}"
                   id="vol-form-phone"
                   className={styles.form__input}
                   placeholder="Phone"
@@ -59,7 +89,19 @@ const VolunteeringForm = ({
                   id="vol-form-email"
                   className={styles.form__input}
                   placeholder="Email"
+                  {...register('email', {
+                    required: true,
+                    pattern: new RegExp(
+                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    ),
+                  })}
                 />
+                {errors.email && (
+                  <p className={styles.form__error__message}>
+                    Polje <i>Email</i> je obavezno za unos i mora biti validnog
+                    email formata
+                  </p>
+                )}
               </div>
               <div className={styles.form__col}>
                 <label className={styles.sr__only} htmlFor="form.message.id">
@@ -69,7 +111,13 @@ const VolunteeringForm = ({
                   id="form.message.id"
                   className={styles.form__textarea}
                   placeholder="Poruka"
+                  {...register('message', { required: true })}
                 ></textarea>
+                {errors.message && (
+                  <p className={styles.form__error__message}>
+                    Polje <i>Poruka</i> je obavezno za unos
+                  </p>
+                )}
                 <div className={styles.form__actions}>
                   {/* TODO poslati mail */}
                   <button
@@ -84,6 +132,7 @@ const VolunteeringForm = ({
           </form>
         </div>
       </div>
+      <Toast />
     </section>
   );
 };
