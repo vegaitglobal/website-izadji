ac215a8caad7f9b1cfe250df7e7b7f8263391f02
Lazar.Jovic <lazar.jovic@doctorcareanywhere.com>
2022-10-28 14:19:43 +0200

    Implement newsletter form submiting. Correct endpoint url needs to be added.

--- izadji/src/components/Newsletter/Newsletter.tsx
+++ izadji/src/components/Newsletter/Newsletter.tsx
@@ -1,4 +1,3 @@
-import { FormEvent } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import ReactMarkdown from 'react-markdown';
@@ -15,20 +14,18 @@ const Newsletter = ({ title, buttonText }: NavBarButtonProps): JSX.Element => {
   const {
     register,
     handleSubmit,
-    getValues,
     formState: { errors },
   } = useForm();
 
   const onSubmit = (data: FieldValues) => {
-    console.log(data);
-    // formService
-    //   .sendEmailData(data)
-    //   .then(() => toast.success('Uspešna prijava.'))
-    //   .catch(() => {
-    //     toast.error(
-    //       'Trenutno postoje smetnje prilikom procesa prijave na newsletter. Molimo, pokušajte ponovo.'
-    //     );
-    //   });
+    formService
+      .applyToNewsletter(data.newsletterEmail)
+      .then(() => toast.success('Uspešna prijava.'))
+      .catch(() => {
+        toast.error(
+          'Trenutno postoje smetnje prilikom procesa prijave na newsletter. Molimo, pokušajte ponovo.'
+        );
+      });
   };
 
   return (
@@ -42,34 +39,38 @@ const Newsletter = ({ title, buttonText }: NavBarButtonProps): JSX.Element => {
             className={`${styles.newsletter__form} ${styles.form}`}
             onSubmit={handleSubmit(onSubmit)}
           >
-            <label htmlFor="newsletter-email" className="sr__only"></label>
-            <input
-              type="text"
-              id="newsletter-email"
-              className={styles.newsletter__input}
-              placeholder="Email Adresa"
-              {...register('email', {
-                required: true,
-                pattern: new RegExp(
-                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                ),
-              })}
-            />
-            {errors.email && (
-              <p className={styles.form__error__message}>
-                Polje <i>Email Adresa</i> je obavezno za unos i mora biti
-                validnog email formata
-              </p>
-            )}
-            <button
-              type="submit"
-              className={`${styles.form__button} ${styles.newsletter__btn} ${styles.btn}`}
-            >
-              {buttonText}
-            </button>
+            <div className={styles.div__col}>
+              <div className={styles.div__row}>
+                <label htmlFor="newsletter-email" className="sr__only"></label>
+                <input
+                  type="text"
+                  id="newsletter-email"
+                  className={styles.newsletter__input}
+                  placeholder="Email Adresa"
+                  {...register('newsletterEmail', {
+                    required: true,
+                    pattern:
+                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  })}
+                />
+                <button
+                  type="submit"
+                  className={`${styles.form__button} ${styles.newsletter__btn} ${styles.btn}`}
+                >
+                  {buttonText}
+                </button>
+              </div>
+              {errors.newsletterEmail && (
+                <p className={styles.newsletter_form_error_message}>
+                  Polje <i>Email Adresa</i> je obavezno za unos i mora biti
+                  validnog email formata
+                </p>
+              )}
+            </div>
           </form>
         </div>
       </div>
+      <Toast />
     </section>
   );
 };
