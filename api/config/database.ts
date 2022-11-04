c73cbb24a73a4556c4fb1d4dbcf571af2f5d4045
m.enjakovic <m.enjakovic@vegait.rs>
2022-11-04 09:50:27 +0100

    Update .env.example

--- api/config/database.ts
+++ api/config/database.ts
@@ -1,18 +1,6 @@
 import path from "path";
 
-export default ({ env }) => env("DATABASE_CLIENT", "sqlite") == "postgres" ? ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'izadji'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'postgres'),
-      ssl: env.bool('DATABASE_SSL', false),
-    },
-  },
-}) : ({
+export default ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
