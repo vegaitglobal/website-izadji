e6ac766bd68565d79c35cea7829a35a4b24a8a9c
Dusica Vasic <d.vasic@vegait.rs>
2023-01-11 11:17:19 +0100

    Reversed change for donate mapper

--- izadji/src/utils/mappers/donatePageMapper.tsx
+++ izadji/src/utils/mappers/donatePageMapper.tsx
@@ -18,7 +18,7 @@ const DonatePageMapper: MappingFunction = (component) => {
             paymentReferenceNumber: p.attributes.paymentReferenceNumber,
             paymentReceiver: p.attributes.paymentReceiver,
             paymentAccountNumber: p.attributes.paymentAccountNumber,
-            projectPageId: p.attributes.id,
+            projectPageId: p.attributes.project_page.data.id,
           }))}
         />
       );
