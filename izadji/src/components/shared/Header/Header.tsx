91ab9b2cee567fae1fe7998078b1acd86c8b3bf1
Tomislav Petkovic <t.petkovic@vegait.rs>
2023-11-22 13:45:02 +0100

    Add header styles for mobile devices

--- izadji/src/components/shared/Header/Header.tsx
+++ izadji/src/components/shared/Header/Header.tsx
@@ -3,6 +3,7 @@ import { routes } from '../../../routes';
 import NavBar, { NavBarProps } from '../../NavBar/NavBar';
 import Icon, { IconProps } from '../Icon/Icon';
 import styles from './Header.module.scss';
+import { useState } from 'react';
 
 export type HeaderProps = {
   emails: string[];
@@ -17,10 +18,14 @@ const Header = ({
   navBar,
   logoSrc,
 }: HeaderProps): JSX.Element => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <div>
-      <header className={styles.header}>
-        <div className={styles.wrap}>
+      <header
+        className={!isMobileMenuOpen ? styles.header : styles.mobile__menu}
+      >
+        <div className={`${styles.wrap} ${styles.header__wrap}`}>
           <div className={styles.header__top}>
             <div className={styles.header__emails}>
               <a href={`mailto:${emails[0]}`}>
@@ -52,22 +57,77 @@ const Header = ({
                 Doniraj
               </Link>
             </div>
-            <button type="button" className={styles.header__hamburger}>
-              <span className={styles.font__ico__menu}></span>
-              <span className={styles.sr__only}>=</span>
-            </button>
           </div>
         </div>
         <div className={styles.header__container}>
-          <Link to="/" className={styles.header__logo}>
-            <img
-              src={logoSrc}
-              alt="Izađi"
-              className={styles.header__logo__img}
-            />
-          </Link>
-          <NavBar buttons={navBar.buttons}></NavBar>
+          <div className={styles.hamburger__logo}>
+            <button
+              className={styles.hamburger__btn}
+              type="button"
+              onClick={() => setMobileMenuOpen((prev) => !prev)}
+            >
+              {isMobileMenuOpen ? (
+                <div className={styles.cross}></div>
+              ) : (
+                <>
+                  <div className={styles.hamburger__line} />
+                  <div className={styles.hamburger__line} />
+                  <div className={styles.hamburger__line} />
+                </>
+              )}
+            </button>
+            {!isMobileMenuOpen ? (
+              <Link to="/" className={styles.header__logo}>
+                <img
+                  src={logoSrc}
+                  alt="Izađi"
+                  className={styles.header__logo__img}
+                />
+              </Link>
+            ) : (
+              <div className={styles.mobile__emails}>
+                <a href={`mailto:${emails[0]}`}>
+                  <span className={styles.header__top__link}>{emails[0]}</span>
+                </a>
+                <a href={`mailto:${emails[1]}`}>
+                  <span className={styles.header__top__link}>{emails[1]}</span>
+                </a>
+              </div>
+            )}
+          </div>
+          {!isMobileMenuOpen && (
+            <Link to={routes.donate} className={styles.header__btn__small}>
+              Doniraj
+            </Link>
+          )}
+
+          <div className={styles.nav__bar}>
+            <NavBar buttons={navBar.buttons}></NavBar>
+          </div>
         </div>
+        {isMobileMenuOpen && (
+          <div>
+            <div className={styles.divider} />
+            <NavBar buttons={navBar.buttons}></NavBar>
+            <ul className={styles.header__social}>
+              {social.map((icon: IconProps & { url: string }) => (
+                <li
+                  key={icon.iconClass}
+                  className={styles.header__social__item}
+                >
+                  <a
+                    className={styles.header__social__link}
+                    href={icon.url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Icon iconClass={icon.iconClass} altText={icon.altText} />
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </header>
     </div>
   );
